7be3d099083a9722a3b723660477185607c749ce
Love4653 <league@iMac.attlocal.net>
2017-04-11 11:58:31 -0700

    lmlkm

--- src/day1/robot/FourSquare.java
+++ src/day1/robot/FourSquare.java
@@ -6,29 +6,44 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-	
+	Robot sarina=new Robot("mini");
 
 	void go() {
+
 		// 4. Make the robot move as fast as possible
-
+sarina.setSpeed(8);
 		// 5. Set the pen width to 5
-
+sarina.setPenWidth(5);
+sarina.penDown();
 		// 6. Do steps #7 to #8 four times...
+for (int i = 0; i < 4; i++) {
+	
 
 			// 7. Set the pen color to random
+	sarina.setRandomPenColor();
 	
 			// 1. Call the drawSquare() method
-	
+	drawSquare();
 			// 8. Turn the robot 90 degrees to the right
+sarina.turn(90);
 
-	}
+
+
+	}}
 
 	/* 3. Fill in the code to draw a square inside the method below. */
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+sarina.move(50);
+sarina.turn(90);
+sarina.move(50);
+sarina.turn(90);
+sarina.move(50);
+sarina.turn(90);
+sarina.move(50);
+sarina.turn(90);
 
-		
-		
+
 	}
 
 	public static void main(String[] args) {
--- src/day1/robot/Spiral.java
+++ src/day1/robot/Spiral.java
@@ -6,22 +6,25 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
+		Robot sarina=new Robot("mini");
 		
 		// 5. Set your robot's pen to the down position
-
+sarina.penDown();
 		// 3. Set the robot to go at max speed (10)
-
+sarina.setSpeed(10);
 		// 4. Do the following (steps 6-9) 75 times
+for (int i = 0; i < 75; i++) {
+	
 
 			// 7. Change the pen color to random
-	
+sarina.setRandomPenColor();
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
-	
+sarina.move(5*i);
 			// 2. Turn the robot 1/3 of 360 degrees to the right
-	
+sarina.move(120);
 			// 8. Change the number of sides to 7 (don’t add a new line of code for this one!)
-	
-			// 9. Set the pen width to i
 
+			// 9. Set the pen width to i
+sarina.setPenWidth(i);
 	}
-}
+}}
