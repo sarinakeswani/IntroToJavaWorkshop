3bd285f335d5af5cea49813c15fe86f6f010b262
Love4653 <league@iMac.attlocal.net>
2017-04-12 11:53:23 -0700

    Day 3

--- src/day3/WorldDomination.java
+++ src/day3/WorldDomination.java
@@ -4,12 +4,22 @@ import javax.swing.JOptionPane;
 
 public class WorldDomination {
 	public static void main(String[] args) {
+		String answer;
+		
 		// 1. Ask the user if they know how to write code.
-
+		answer=JOptionPane.showInputDialog("Do you speak code?");
+		if (answer.equals(anObject))
 		// 2. If they say "yes", tell them they will rule the world.
-
+		{JOptionPane.showInputDialog("You will rule the world");
 		// 3. Otherwise, wish them good luck washing dishes.
 
+	}
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
 
---
+++ src/day3/WorldsNicestProgram.java
@@ -0,0 +1,18 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class WorldsNicestProgram {
+	
+	public static void main(String[] args) {
+		String name;
+		JOptionPane.showMessageDialog(null, "You are funny!");
+		JOptionPane.showMessageDialog(null, "You are very kind!");
+		JOptionPane.showMessageDialog(null, "You are friendly!");
+		name=JOptionPane.showInputDialog("Enter Your Name");	
+		//syso(name);
+		JOptionPane.showMessageDialog(null, "Hello "+name);
+		
+	}
+
+}
